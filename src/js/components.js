d0206dd54f42006eb1781ab5373b97fe6c731830
Jack Greenberg <theprogrammerjack@gmail.com>
2017-08-20 19:04:25 +0000

    Refactor all of the main.js code into the `moon` component.

--- src/js/components.js
+++ src/js/components.js
@@ -27,3 +27,57 @@ AFRAME.registerComponent('orbit', {
         this.orbits = orbits;
     }
 });
+
+AFRAME.registerComponent('moon', {
+    dependencies: ['position', 'camera','look-controls'],
+    init: function() {
+        this.d = Math.PI * 2 / (10000/10);
+        this.state = 0;
+        this.time = 0;
+        this.angle = 0;
+        this.movement = {};
+        this.el.querySelector('a-entity[cursor]').addEventListener('click', (evt) => {
+            console.log(evt);
+            let intersection = evt.detail.intersection;
+            let pos = this.el.getAttribute('position');
+            let time = intersection.distance * 1000 / 15.7;
+            let m = {}
+            m.x = (intersection.point.x - pos.x) / (time / 10);
+            m.y = (intersection.point.y - pos.y) / (time / 10);
+            m.z = (intersection.point.z - pos.z) / (time / 10);
+            m.dest = intersection.point;
+            this.movement = m;
+            this.state = 1;
+        });
+    },
+    tick: function(t) {
+        if (t - this.time < 10) return;
+        let c = this.el;
+        switch (this.state) {
+            case 0:
+                let xy = c.getAttribute('position');
+                let loc = this.computeLoc(xy.x, xy.y, this.angle);
+                this.angle = loc.a;
+                c.setAttribute('position', {x: loc.x, y: loc.y});
+                break;
+            case 1:
+                let m = this.movement;
+                let xyz = c.getAttribute('position');
+                c.setAttribute('position', {x: xyz.x + m.x, y: xyz.y + m.y, z: xyz.z + m.z});
+                // if (movement.dest.x > c.getAttribute('position').x || movement.dest.y > c.getAttribute('position').y || movement.dest.z > c.getAttribute('position').z) {
+                //     console.log("hit!");
+                //     state = 0;
+                // }
+                break;
+        }
+    },
+    computeLoc: function(x, y, a) {
+        let angle = (a + this.d) % (2 * Math.PI);
+    
+        return {
+            a: angle,
+            x: 50 * Math.cos(angle),
+            y: 50 * Math.sin(angle),
+        }
+    }
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,56 @@
 
 
-var playing = true;
+// var playing = true;
 
-var c = document.querySelector('a-entity[camera]'); //the camera
-console.log(c.components);
-// c.setAttribute('position', '50 0 4');
-console.log(c.getAttribute('position'));
+// //Game state: 0 -> orbiting, 1 -> transferring between orbits
+// var state = 0;
 
-var d = Math.PI * 2 / (10000/10);
-var angle = 0;
-//game loop
-var gameLoop = setInterval(() => {
-    let xy = c.getAttribute('position');
-    let loc = computeLoc(xy.x, xy.y, angle);
-    angle = loc.a;
-    c.setAttribute('position', {x: loc.x, y: loc.y})
-}, 10);
+// var movement = {};
 
-var computeLoc = (x, y, a) => {
-    let angle = (a + d) % (2 * Math.PI);
+// var c = document.querySelector('a-entity[camera]'); //the camera
+// console.log(c.components);
+// document.querySelector('a-entity[cursor]').addEventListener('click', (evt) => {
+//     console.log(evt);
+//     let intersection = evt.detail.intersection;
+//     let pos = c.getAttribute('position');
+//     let time = intersection.distance * 1000 / 15.7;
+//     movement.x = (intersection.point.x - pos.x) / (time / 10);
+//     movement.y = (intersection.point.y - pos.y) / (time / 10);
+//     movement.z = (intersection.point.z - pos.z) / (time / 10);
+//     movement.dest = intersection.point;
+//     state = 1;
+// });
+// // c.setAttribute('position', '50 0 4');
+// console.log(c.getAttribute('position'));
 
-    return {
-        a: angle,
-        x: 50 * Math.cos(angle),
-        y: 50 * Math.sin(angle),
-    }
-}
+// var d = Math.PI * 2 / (10000/10);
+// var angle = 0;
+// //game loop
+// var gameLoop = setInterval(() => {
+//     switch (state) {
+//         case 0:
+//             let xy = c.getAttribute('position');
+//             let loc = computeLoc(xy.x, xy.y, angle);
+//             angle = loc.a;
+//             c.setAttribute('position', {x: loc.x, y: loc.y});
+//             break;
+//         case 1:
+//             let xyz = c.getAttribute('position');
+//             c.setAttribute('position', {x: xyz.x + movement.x, y: xyz.y + movement.y, z: xyz.z + movement.z});
+//             // if (movement.dest.x > c.getAttribute('position').x || movement.dest.y > c.getAttribute('position').y || movement.dest.z > c.getAttribute('position').z) {
+//             //     console.log("hit!");
+//             //     state = 0;
+//             // }
+//             break;
+//     }
+// }, 10);
+
+// var computeLoc = (x, y, a) => {
+//     let angle = (a + d) % (2 * Math.PI);
+
+//     return {
+//         a: angle,
+//         x: 50 * Math.cos(angle),
+//         y: 50 * Math.sin(angle),
+//     }
+// }
